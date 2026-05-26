89a8d19a1ffc577e37d7f43f23374256ea18a623
Marco Gomes <143396504+marco-igomes@users.noreply.github.com>
2026-05-27 00:01:28 +0100

    feat(firewall_policy): add ip_group_id to source/destination structs

--- unifi/firewall_policy.generated.go
+++ unifi/firewall_policy.generated.go
@@ -81,6 +81,7 @@ func (dst *FirewallPolicy) UnmarshalJSON(b []byte) error {
 }
 
 type FirewallPolicyDestination struct {
+	IPGroupID          string   `json:"ip_group_id,omitempty"` // firewall_group ID when matching_target=IP and matching_target_type=OBJECT
 	IPs                []string `json:"ips,omitempty"`
 	MatchMAC           bool     `json:"match_mac"`
 	MatchOppositeIPs   bool     `json:"match_opposite_ips"`
@@ -145,6 +146,7 @@ func (dst *FirewallPolicySchedule) UnmarshalJSON(b []byte) error {
 }
 
 type FirewallPolicySource struct {
+	IPGroupID          string   `json:"ip_group_id,omitempty"` // firewall_group ID when matching_target=IP and matching_target_type=OBJECT
 	IPs                []string `json:"ips,omitempty"`
 	MatchMAC           bool     `json:"match_mac"`
 	MatchOppositeIPs   bool     `json:"match_opposite_ips"`
